701734fbb0328bbc42b80e9bcdd8a81d1056561a
now-eat-krap <916ss@naver.com>
2024-08-09 15:14:40 +0900

    다크모드 구현 완료

--- static/function.js
+++ static/function.js
@@ -1,14 +1,37 @@
-function darkMode(){
+const button = document.querySelector('.mode-change-btn')
+const icon = document.querySelector('i')
+const userTheme = localStorage.getItem('theme')
+let status = false
 
-  // 현재 모드를 가져옴
-  let mode = $('html').attr("data-bs-theme");
+// 처음 이용객의 테마를 읽음
+document.addEventListener('DOMContentLoaded', () => {
+    if (userTheme === 'dark') {
+        clickDarkMode()
+    } else if (userTheme === 'light') {
+        clickLightMode()
+    }
+})
 
-  // 현재 모드와 반대되는 모드로 설정
-  if( mode == 'dark' ){
-    $('html').attr("data-bs-theme", "light");
-    $('.mode-change-btn').html('다크모드');
-  }else{
-    $('html').attr("data-bs-theme", "dark");
-    $('.mode-change-btn').html('라이트모드');
-  }
+// 버튼클릭
+button.addEventListener('click', () => {
+    if (status === false) {
+        clickDarkMode()
+    } else if (status === true) {
+        clickLightMode()
+    }
+})
+
+// 다크/라이트 전환이벤트
+function clickDarkMode() {
+    localStorage.setItem("theme", "dark")
+    icon.className = "bi bi-moon h4"
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+    status = true
+}
+function clickLightMode() {
+    localStorage.setItem("theme", "light")
+    icon.className = "bi bi-sun h4"
+    //button.innerText='<i class="bi bi-sun h4"></i>'
+    document.documentElement.setAttribute('data-bs-theme', 'light')
+    status = false
 }
